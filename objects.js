35831cdd611b9d9f4b9eb576ac25d3b14d603a28
Carriane <carrianelinzer@icloud.com>
2022-09-09 18:25:29 -0500

    slider button

--- objects.js
+++ objects.js
@@ -1,24 +1,22 @@
 let myLibrary = [];
 
 const book = {
-    title: 'Default',
-    author: 'Default',
-    pages: 'Default',
-    read: function() {
-        return `I have read ${this.title} by ${this.author}, which is ${this.pages} pages long.`;
-    }
+    title: '',
+    author: '',
+    pages: '',
+    // read: function() {
+    //     return `I have read ${this.title} by ${this.author}, which is ${this.pages} pages long.`;
+    // }
 };
 
 function addBookToLibrary() {
-    const myBooks = myLibrary.push(Book);
+   const newBook = new book();
+   newBook.title = bookForm.title.value;
+   newBook.author = bookForm.author.value;
+   newBook.pages = bookForm.pages.value;
+   myLibrary.push(newBook);
+   
 }
 
-for (let i = 0; i < myLibrary.length; i++) {
-    return myLibrary;
-};
 
-const mybook = Object.create(book);
-mybook.title = 'Green Eggs and Ham';
-mybook.author = 'Dr.Suess';
-mybook.pages = '14';
-console.log(mybook.read());
+console.log(myLibrary(newBook));
